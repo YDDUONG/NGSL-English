ba6cdc90c7f0fc3eeccdd9f0b345ca5ea3b171a2
YDDUONG <ydduong@163.com>
2020-02-12 12:49:55 +0800

    优化

--- app/src/main/java/com/example/ngsl/room/AppDatabase.kt
+++ app/src/main/java/com/example/ngsl/room/AppDatabase.kt
@@ -9,10 +9,24 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.ngsl.DATABASE_NAME
 
-@Database(entities = [Word::class] , version = 1, exportSchema = false)
+@Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
 
+//    companion object {
+//        @Volatile
+//        private var instance: AppDatabase? = null
+//
+//        fun getInstance(context: Context): AppDatabase {
+//            return instance ?: synchronized(this) {
+//                instance ?: Room.databaseBuilder(
+//                    context,
+//                    AppDatabase::class.java,
+//                    DATABASE_NAME
+//                ).build().also { instance = it }
+//            }
+//        }
+//    }
     companion object {
 
         // For Singleton instantiation
--- app/src/main/java/com/example/ngsl/wordFragment/WordViewModel.kt
+++ app/src/main/java/com/example/ngsl/wordFragment/WordViewModel.kt
@@ -8,10 +8,13 @@ import com.example.ngsl.room.WordRepository
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
     //声明repository
-    val allWord: LiveData<List<Word>>
-    init {
-        //word仓库
-        val wordRepository = WordRepository.getInstance(application.applicationContext)
-        allWord = wordRepository.allWord
+    private val _allWord: LiveData<List<Word>> by lazy {
+        WordRepository.getInstance(application.applicationContext).allWord
     }
+    //    init {
+//        //word仓库
+//        val wordRepository = WordRepository.getInstance(application.applicationContext)
+//        _allWord = wordRepository.allWord
+//    }
+    val allWord: LiveData<List<Word>> get() = _allWord
 }
